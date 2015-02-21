18bfcbe0d5eab9aefabf4b616ffaf414d6a2ca41
Nate Armstrong <natearmstrong2@gmail.com>
2015-02-21 15:31:53 -0700

    first commit: empty tabbed bar application.

---
+++ Caret/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Caret
+//
+//  Created by Nate Armstrong on 2/21/15.
+//  Copyright (c) 2015 Nate Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
